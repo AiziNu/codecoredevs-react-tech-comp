c1189835e0c7492fd98e8961ab6e7131a5457347
AiziNu <aizireknuralieva0@gmail.com>
2024-05-15 19:42:33 +0200

    initial commit

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import HomePage from "../HomePage";
+import Card from "../Card";
+import Footer from "../Footer";
+
+function Home() {
+  return (
+    <>
+      <HomePage />
+      <Card />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
