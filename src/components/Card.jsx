ec956e5fa64ade33387e74d1dfabc863a1a1e758
AiziNu <aizireknuralieva0@gmail.com>
2024-05-15 19:47:18 +0200

    Small changes in Branch CT-158

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,7 +5,7 @@ import "./Card.css";
 function Card() {
   return (
     <div className="cards">
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Check out Projects amd our Services!</h1>
       <div className="cards__container">
         <div className="cards__wrapper">
           <ul className="cards__items">
